31915de808d22dfeae97cbf4c2379d7c2c1a469b
Brandon Quach <bquach@caltech.edu>
2019-03-09 15:08:53 -0800

    second push

--- preprocessing.py
+++ preprocessing.py
@@ -2,5 +2,5 @@ import os
 import re
 import HMM_helper
 
-text = open(os.path.join(os.getcwd(), 'test.txt')).read()
-HMM_helper.parse_observations(text)
+text = open(os.path.join(os.getcwd(), 'data/shakespeare.txt')).read()
+obs, obs_map = HMM_helper.parse_observations(text)
